8182f1bc05d3423045c13a441aaf7a7e62ac9b98
Abdul Ghofar <ofaasd@gmail.com>
2025-01-22 10:34:39 +0700

    update is verifikasi to 1

--- application/models/Model_online.php
+++ application/models/Model_online.php
@@ -496,7 +496,7 @@
 				$set_nopen = $prodi->kode . $ta . $gelombang->no_gel . $new_id;
 				$data = array(
 						'nopen' => $set_nopen,
-						'is_verifikasi' => 1,
+						//'is_verifikasi' => 1, Buka jika sudah diijinkan
 					 );
 				$r = $this->db->update('pmb_peserta_online', $data, ['id'=>$pmb_online->id]);
 				$data_user = array(
